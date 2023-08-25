ee2e3dd5e0b67fbe7edf93bae6851ca279141496
박성경 <sungkyung@itsdcode.com>
2023-08-25 14:15:26 +0900

    (feat): mongodb ts-ignore 추가

--- src/util/database.tsx
+++ src/util/database.tsx
@@ -7,10 +7,13 @@ const options: MongoClientOptions = {};
 let connectDB: Promise<MongoClient>;
 
 if (process.env.NODE_ENV === 'development') {
+  // @ts-ignore
   if (!global._mongo) {
+    // @ts-ignore
     global._mongo = new MongoClient(url, options).connect();
   }
 
+  // @ts-ignore
   connectDB = global._mongo;
 } else {
   connectDB = new MongoClient(url, options).connect();
